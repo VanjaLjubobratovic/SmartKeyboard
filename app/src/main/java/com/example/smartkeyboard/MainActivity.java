19195ba1d2b4c437f630586664cb55ba8f93147e
Vanja Ljubobratovic <vanja1708@gmail.com>
2022-12-19 21:24:35 +0100

    Implemented basic toolbar. No actions upon clicking yet.

--- app/src/main/java/com/example/smartkeyboard/MainActivity.java
+++ app/src/main/java/com/example/smartkeyboard/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.smartkeyboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.showOverflowMenu();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        final String TAG = "MENU";
+
+        switch (item.getItemId()){
+            case R.id.logSettings:
+                //TODO: Open logging settings
+                Log.d(TAG, "onOptionsItemSelected: SESSION");
+                return true;
+
+            case R.id.sessionSettings:
+                //TODO: Open session settings
+                Log.d(TAG, "onOptionsItemSelected: SESSION");
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
