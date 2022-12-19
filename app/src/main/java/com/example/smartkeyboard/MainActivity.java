f6fee657f4e8348f02f9e178c636cdad0e8a7b86
Vanja Ljubobratovic <vanja1708@gmail.com>
2022-12-19 23:10:31 +0100

    Implemented basic MainActivity UI. Implemented phrases file reading.

--- app/src/main/java/com/example/smartkeyboard/MainActivity.java
+++ app/src/main/java/com/example/smartkeyboard/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.smartkeyboard;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialButton generateBtn;
+    private BufferedReader reader;
+    private ArrayList<String> phrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +39,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.myToolbar);
         setSupportActionBar(myToolbar);
         myToolbar.showOverflowMenu();
+
+        try {
+            InputStream inputStream = getResources().openRawResource(R.raw.phrases2);
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = reader.readLine();
+
+            while(line != null) {
+                phrases.add(line);
+                line = reader.readLine();
+
+                Log.d("FILE_READ", "Read line: " + line);
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("FILE_READ", "Phrases file not found!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("FILE_READ", "Error reading file");
+            e.printStackTrace();
+        }
+
+        generateBtn = findViewById(R.id.phraseGenerateBtn);
+
+        generateBtn.setOnClickListener(view -> {
+
+        });
     }
 
     @Override
